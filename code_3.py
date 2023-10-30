43885099c91d1f50989461ee9e42ff3f5c156e6d
IgnatVolk <142455386+IgnatVolk@users.noreply.github.com>
2023-10-30 23:00:58 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,52 @@
+    for item in data:
+
+    return data
+
+if __name__ == "__main__":
+import random
+def main():
+    data = generate_random_data()
+
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+    main()if __name__ == "__main__":
+        print(f"Random Number: {item}")
+import random
+def main():
+    main()
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+
+    for item in data:
+    data = generate_random_data()
+    return data
+def generate_random_data():
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+
+def main():
+    data = generate_random_data()
+    main()
+if __name__ == "__main__":
+
+    return data
+
+import random
+        print(f"Random Number: {item}")
+    public static void main(String[] args) {
+
+        for (int item : data) {
+}
+    }
+            System.out.println("Random Number: " + item);
+
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+        }
